6979b9d182db2ba55bfdb7d6325295ac223c683e
benthen <nanhui0508@gmail.com>
2024-01-16 11:08:00 +0800

    second commit

--- ATM-Simulator-System/src/ASimulatorSystem/BalanceEquiry.java
+++ ATM-Simulator-System/src/ASimulatorSystem/BalanceEquiry.java
@@ -12,10 +12,6 @@ class BalanceEnquiry extends JFrame implements ActionListener {
     JButton b1, b2, b3;
     JLabel l1, l2, l3;
     String pin;
-    String address = "Malaysia";
-    int postcode = 12345;
-    String name ="Ben";
-    int age = 0;
 
     BalanceEnquiry(String pin) {
         this.pin = pin;
